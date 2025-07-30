bbdf8a7beca52cf517083742cd1f7999cc81ba43
biruk lemma <biruklemmadebela@gmail.com>
2025-07-30 18:32:32 +0300

    Remove unused import statement for route in login.tsx to clean up code.

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -10,7 +10,7 @@ import { Checkbox } from "@/components/ui/checkbox"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import AuthLayout from "@/layouts/auth-layout"
-import { route } from "ziggy-js" // Import route from ziggy-js
+// import { route } from "ziggy-js" // Import route from ziggy-js
 
 type LoginForm = {
   email: string
