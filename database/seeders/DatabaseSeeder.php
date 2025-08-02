acd6aacec1a1527d61c38ffe8402b53751bf9383
biruk lemma <biruklemmadebela@gmail.com>
2025-08-02 21:10:05 +0300

    Add password and email verification to test user in DatabaseSeeder
    - Updated the DatabaseSeeder to include a hashed password and set the email_verified_at timestamp for the test user.
    - This ensures the test user is fully initialized for testing purposes.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Carbon\Carbon;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -29,6 +30,8 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => Hash::make('test1234'),
+            'email_verified_at' => Carbon::now()
         ]);
 
        $admin =  User::factory()->create([
