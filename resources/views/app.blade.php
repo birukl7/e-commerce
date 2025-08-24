7e8de7e56050df0d450d24f1d5e8781ddf336541
biruk <biruklemmadebela@gmail.com>
2025-08-24 10:08:41 +0300

    fix: changed app name

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,7 @@
             }
         </style>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Serdo') }}</title>
 
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
