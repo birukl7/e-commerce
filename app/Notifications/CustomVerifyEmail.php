6ad6351ab93d851ba2bd21837934e78a98073ff7
Dechasa <Dechu373@protonmail.com>
2025-11-21 14:15:26 +0300

    afdsaf

--- app/Notifications/CustomVerifyEmail.php
+++ app/Notifications/CustomVerifyEmail.php
@@ -4,15 +4,35 @@ namespace App\Notifications;
 
 use Illuminate\Auth\Notifications\VerifyEmail as BaseVerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\URL;
 
 class CustomVerifyEmail extends BaseVerifyEmail
 {
+    /**
+     * Get the verification URL for the given notifiable.
+     * 
+     * Override to extend expiration from 60 minutes to 7 days.
+     *
+     * @param  mixed  $notifiable
+     * @return string
+     */
+    protected function verificationUrl($notifiable)
+    {
+        return URL::temporarySignedRoute(
+            'verification.verify',
+            now()->addDays(7), // 7 days instead of default 60 minutes
+            [
+                'id' => $notifiable->getKey(),
+                'hash' => sha1($notifiable->getEmailForVerification()),
+            ]
+        );
+    }
+
     /**
      * Build the mail message.
      */
     public function toMail($notifiable)
     {
-        // ✅ now works, because BaseVerifyEmail provides verificationUrl()
         $url = $this->verificationUrl($notifiable);
 
         return (new MailMessage)
